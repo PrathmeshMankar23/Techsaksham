7f79c4cb49fd6b6a9422571ff7d84ed4c174e3f8
PrathmeshMankar23 <Prathmeshmankar1023@gmail.com>
2024-09-27 16:06:21 +0530

    commit 1

---
+++ FUNCTIONS/callingfunction.js
@@ -0,0 +1,14 @@
+/*function msg(){
+   let greeting ="welcome to csbs";
+   let num=10
+   return greeting," ",+num;
+}
+
+console.log(msg());
+*/
+let num1;
+let num2;
+function addition(num1,num2){
+    return num1+num2;
+}
+console.log(addition(23,45))
